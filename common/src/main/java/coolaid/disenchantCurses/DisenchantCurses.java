1c851c1e8d3b6bc74a7ad05dd33ad84954f00aff
CoolAid48 <atgunsel@gmail.com>
2026-01-19 11:36:37 -0500

    Start of multiloader support dev

---
+++ common/src/main/java/coolaid/disenchantCurses/DisenchantCurses.java
@@ -0,0 +1,9 @@
+package coolaid.disenchantCurses;
+
+public final class DisenchantCurses {
+    public static final String MOD_ID = "disenchant-curses";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/coolaid/disenchantCurses/neoforge/DisenchantCursesNeoForge.java
@@ -0,0 +1,12 @@
+package coolaid.disenchantCurses.neoforge;
+
+import coolaid.disenchantCurses.DisenchantCurses;
+import net.neoforged.fml.common.Mod;
+
+@Mod(DisenchantCurses.MOD_ID)
+public final class DisenchantCursesNeoForge {
+    public DisenchantCursesNeoForge() {
+        // Run our common setup.
+        DisenchantCurses.init();
+    }
+}
